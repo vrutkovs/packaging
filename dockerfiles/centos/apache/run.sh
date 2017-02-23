ad5cfe277077a2935f14ba654a0acfb4ba5926a7
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-02-23 18:34:16 +0100

    Don't regenerate additional cert

--- dockerfiles/centos/apache/run.sh
+++ dockerfiles/centos/apache/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -x
 
 # re-create the dummy cert with the correct hostname
 pushd /etc/pki/tls/certs/
+if [ ! -e localhost.crt ]; then
 rm localhost.crt
 cat << EOF | make testcert
 .
@@ -12,6 +14,7 @@ cat << EOF | make testcert
 pulpapi
 .
 EOF
+fi
 popd
 
 exec httpd -D FOREGROUND
